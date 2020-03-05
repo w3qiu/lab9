c08c4756eda277c584bf60093036619dd83ccb83
w3qiu <damon.q26@gmail.com>
2020-03-05 14:59:12 -0800

    finish lab9

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,22 @@ var labs = require('../labs.json');
 
 exports.view = function(req, res){
   if(labs.length < 9) {
-  	var todaysLab = [{
+      
+  	var todaysLab = {
 		"id": "lab-9",
 		"title": "Debugging 101",
 		"date": "March 7",
 		"image": "debug.png"
-	}];
+	};
 
+      
   	labs.push(todaysLab);
+      
   }
 
   res.render('index', {
+             
 	'labs': labs
+             
   });
 };
